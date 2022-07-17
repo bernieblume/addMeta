ff8e33385e05ce9ff306ca5622e768528272c8b1
Bernie Blume <bernie.blume@antsle.com>
2022-07-17 13:22:47 -0700

    Initial commit.

---
+++ programs/addmeta/src/lib.rs
@@ -0,0 +1,107 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{TokenAccount};
+use solana_program::program::invoke_signed;
+use std::convert::Into;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token},
+   };
+
+use mpl_token_metadata::instruction::create_metadata_accounts_v3;
+// use mpl_token_metadata::pda::find_metadata_account;
+
+pub const CTRLSEED: &[u8] = b"CTRLv1";
+
+declare_id!("3WEXkDmTYHfGjLWnFW2NoAhgEcDgcPn2LHKBPGLUcgq2");
+
+#[program]
+mod addmeta {
+    use super::*;
+
+    pub fn mint_ctrl(ctx: Context<MintCtrl>, bump: u8, amount: u64) -> Result<()> {
+        anchor_spl::token::mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::MintTo {
+                    mint: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.destination.to_account_info(),
+                    authority: ctx.accounts.mint.to_account_info(),
+                },
+                &[&[CTRLSEED, &[bump]]],
+            ),amount,
+        )?;
+        Ok(())
+    }    
+
+    pub fn tok_meta(ctx: Context<TokMeta>, bump: u8) -> Result<()> {
+       let ix = create_metadata_accounts_v3(
+            mpl_token_metadata::id(), // program_id,
+            *ctx.accounts.metadata_pda.to_account_info().key, // metadata_account,
+            *ctx.accounts.mint.to_account_info().key, //mint,
+            *ctx.accounts.mint.to_account_info().key, //mint_authority,
+            *ctx.accounts.payer.to_account_info().key, //payer,
+            *ctx.accounts.updauth.to_account_info().key, //update_authority,
+            String::from("CTRL - Program Controlled Solana SPL Token"), // name,
+            String::from("CTRL"), // symbol,
+            String::from("https://bernieblume.github.io/Ctrl/Ctrl.json"), // uri,
+            None, // creators,
+            0u16, //seller_fee_basis_points,
+            false, // update_authority_is_signer,
+            true, // is_mutable,
+            None, // collection,
+            None, // uses,
+            None, // collection_details
+        );
+        invoke_signed(
+            &ix,
+            &[
+                ctx.accounts.metadata_pda.to_account_info().clone(), // Metadata account
+                ctx.accounts.mint.to_account_info().clone(), // Mint
+                ctx.accounts.mint.to_account_info().clone(), // Mint Authority
+                ctx.accounts.payer.to_account_info().clone(), // Payer
+                ctx.accounts.updauth.to_account_info().clone(), // Update Authority
+                ctx.accounts.system_program.to_account_info().clone(), // System Program
+                ctx.accounts.rent.to_account_info().clone(), // Rent Sysvar
+            ],
+            &[
+                &[CTRLSEED.as_ref(), &[bump]],
+            ],
+        )?;
+        Ok(())
+    }
+
+}
+
+#[derive(Accounts)]
+#[instruction(okubump: u8, amount: u64)]
+pub struct MintCtrl<'info> {
+    #[account(init_if_needed, payer = payer, seeds = [CTRLSEED.as_ref()], bump, mint::decimals = 6, mint::authority = mint)]
+    pub mint: Account<'info, Mint>,
+    #[account(init_if_needed, payer = payer, associated_token::mint = mint, associated_token::authority = payer)]
+    pub destination: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+#[instruction(bump: u8)]
+pub struct TokMeta<'info> {
+    #[account(seeds = [CTRLSEED.as_ref()], bump = bump, mint::decimals = 6, mint::authority = mint)]
+    pub mint: Account<'info, Mint>,
+    /// CHECK: This is not dangerous because it will be checked in the inner instruction
+    pub mintauth: AccountInfo<'info>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    /// CHECK: This is not dangerous because it will be checked in the inner instruction
+    pub updauth: AccountInfo<'info>,
+    /// CHECK: This is not dangerous because it's being checked by the inner instruction
+    #[account(mut)]
+    pub metadata_pda: AccountInfo <'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
---
+++ tests/addmeta.js
@@ -0,0 +1,77 @@
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram, PublicKey, SYSVAR_RENT_PUBKEY } = anchor.web3;
+const { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, getAssociatedTokenAddress } = require("@solana/spl-token");
+const { findMetadataPda } = require("@metaplex-foundation/js");
+
+const CTRLSEED = 'CTRLv1';
+const ADMINACC = "Ec3nzEVcQ7jxgJWoHF9eECa2ysyjK95h6agNQVesNXnK";  // Replace with YOUR wallet address
+
+describe("addmeta", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  usr = provider.wallet.publicKey;
+  console.log("Endpoint:", provider.connection.rpcEndpoint);
+  const program = anchor.workspace.Addmeta;
+
+  it("Gets some CTRL", async () => {
+
+    const [ctrlPDA, bump] = await PublicKey.findProgramAddress([anchor.utils.bytes.utf8.encode(CTRLSEED)], program.programId);
+    console.log("GetCTRL: ctrlPDA is [", ctrlPDA.toString(), "], bump ", bump);
+
+    let receiver = usr;
+    let assocTokAcct = await getAssociatedTokenAddress(
+      ctrlPDA,
+      receiver,
+      true, // allowOwnerOffCurve
+      TOKEN_PROGRAM_ID,
+      ASSOCIATED_TOKEN_PROGRAM_ID,
+    );
+    console.log("GetCTRL: AssocTokAcct: ", assocTokAcct.toString());
+
+    let bigAmount = new anchor.BN(888 * 1000000);
+
+    await program.methods
+      .mintCtrl(bump, bigAmount)
+      .accounts({
+        mint: ctrlPDA,
+        destination: assocTokAcct,
+        payer: usr,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+      }).rpc();
+
+    console.log("GetCTRL done.");
+    assert.ok(true);
+  });
+
+  it("Sets the token metadata", async () => {
+    const [ctrlPDA, bump] = await PublicKey.findProgramAddress([anchor.utils.bytes.utf8.encode(CTRLSEED)], program.programId);
+    console.log("TokMeta: okuPDA is [", ctrlPDA.toString(), "], bump ", bump);
+    const metadataPDA = await findMetadataPda(ctrlPDA);
+    console.log("TokMeta: metadataPDA is [", metadataPDA.toString(), "]");
+    const adm = new PublicKey(ADMINACC);
+    console.log("TokMeta: usr=", usr.toString(), "adm=", adm.toString())
+
+    await program.methods
+      .tokMeta(bump)
+      .accounts({
+        mint: ctrlPDA,
+        mintauth: ctrlPDA,
+        payer: usr,
+        updauth: adm,
+        metadataPda: metadataPDA,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+      })
+      .rpc();
+
+      console.log("TokMeta done.");
+    assert.ok(true);
+  });
+
+
+ });
